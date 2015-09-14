544a7202031241ee78c3b42083b493d9cf5711e0
Glen Langer <contao.modul@glen-langer.de>
2015-09-15 00:21:37 +0200

    Fixed #3, Update Transifex Language Files

--- src/Resources/contao/languages/de/tl_xing_category.php
+++ src/Resources/contao/languages/de/tl_xing_category.php
@@ -3,12 +3,12 @@
  * Translations are managed using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/contao-xing/language/de/
+ * @link http://docs.transifex.com/faq/#translating
+ * @link https://www.transifex.com/projects/p/contao-xing-bundle/language/de/
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-01-26T23:50:57+01:00
+ * last-updated: 2015-09-14T23:16:16+02:00
  */
 
 
--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -3,17 +3,17 @@
  * Translations are managed using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/contao-xing/language/en/
+ * @link http://docs.transifex.com/faq/#translating
+ * @link https://www.transifex.com/projects/p/contao-xing-bundle/language/en/
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-01-26T23:50:57+01:00
+ * last-updated: 2015-09-14T23:16:16+02:00
  */
 
 
 $GLOBALS['TL_LANG']['FMD']['xinglist']['0'] = 'XING list';
 $GLOBALS['TL_LANG']['FMD']['xinglist']['1'] = 'With this module you can list XING-Banners in the front end.';
-$GLOBALS['TL_LANG']['MOD']['xing']['0']  = 'XING';
-$GLOBALS['TL_LANG']['MOD']['xing']['1']  = 'This module allows you to manage XING-Banners.';
+$GLOBALS['TL_LANG']['MOD']['xing']['0']     = 'XING';
+$GLOBALS['TL_LANG']['MOD']['xing']['1']     = 'This module allows you to manage XING-Banners.';
 
