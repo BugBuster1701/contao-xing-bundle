ef0d2d8112cbc93de7a3e99279ae5bf74e8ec590
Glen Langer <contao.modul@glen-langer.de>
2017-03-26 00:50:54 +0100

    #5, Anpassungen an Contao 4.3 Managed Edition

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @copyright  Glen Langer 2008..2017 <http://contao.ninja>
+ * @author     Glen Langer (BugBuster)
+ * @package    Xing
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/BugBuster1701/contao-xing-bundle
+ *
+ */
+
+namespace BugBuster\XingBundle\ContaoManager;
+
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+/**
+ * Plugin for the Contao Manager.
+ *
+ * @author Glen Langer (BugBuster)
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create('BugBuster\XingBundle\BugBusterXingBundle')
+                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle'])
+                ->setReplace(['xing']),
+        ];
+    }
+}
