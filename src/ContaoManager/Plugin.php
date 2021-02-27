f95e44920756b2da0e763a32f56545001ec4dde2
Glen Langer <contao.modul@glen-langer.de>
2021-02-27 21:36:33 +0100

    run php-cs-fixer

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of a BugBuster Contao Bundle
  *
- * @copyright  Glen Langer 2020 <http://contao.ninja>
+ * @copyright  Glen Langer 2021 <http://contao.ninja>
  * @author     Glen Langer (BugBuster)
  * @package    Xing
  * @license    LGPL-3.0-or-later
