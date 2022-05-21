2eef5bfbbdaf0e9057c6e8acdae69953c0ad2c4a
Glen Langer <contao.modul@glen-langer.de>
2022-05-21 23:48:00 +0200

    Fixed #19, PHP Warning: Undefined array key "deleteConfirm"

--- src/Resources/contao/dca/tl_xing.php
+++ src/Resources/contao/dca/tl_xing.php
@@ -86,7 +86,7 @@ $GLOBALS['TL_DCA']['tl_xing'] = array
 				'label'               => &$GLOBALS['TL_LANG']['tl_xing']['delete'],
 				'href'                => 'act=delete',
 				'icon'                => 'delete.gif',
-				'attributes'          => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"'
+				'attributes'          => 'onclick="if (!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null ) . '\')) return false; Backend.getScrollOffset();"'
 			),
 			'toggle' => array
 			(
