883890d8677df1d1561340c378ca35b47635c8b3
Glen Langer <contao.modul@glen-langer.de>
2025-10-03 18:40:19 +0200

    #29, Add an fix depcheck, add phpstan

---
+++ depcheck.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+$config = new Configuration();
+
+return $config
+    //// Ignoring errors
+    ->ignoreErrors([ErrorType::DEV_DEPENDENCY_IN_PROD]);
